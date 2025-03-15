c0ca24371811328004e22f19ec03a659812e05ca
egorvania1 <egorvania1@gmail.com>
2025-03-15 12:55:53 +0500

    Спрайт монеток, звук монеток, папки для всякой всячины, нормальное размещение UI

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,11 @@
 extends Area2D
 
+@onready var anim = %AnimationPlayer
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player": 
+		anim.play("collect")
 		body.add_point()
-		queue_free()
-	else: print_debug("Not player")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "collect": queue_free()
