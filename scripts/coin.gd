fc90200377f33d82ea9e7539ccbe0e4d0a2682b5
egorvania1 <egorvania1@gmail.com>
2025-03-15 13:15:44 +0500

    кхм

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -8,4 +8,9 @@ func _on_body_entered(body: Node2D) -> void:
 		body.add_point()
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "collect": queue_free()
+	match anim_name:
+		"collect": queue_free()
+		"blink": queue_free()
+
+func _on_life_timer_timeout() -> void:
+	anim.play("blink")
